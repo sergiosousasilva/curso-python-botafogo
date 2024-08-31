67848e78be014bc8e577ea0af931fc4c3e13f972
Fabiano <professor@fabianomoreira.com.br>
2024-08-30 21:33:25 -0300

    Links e Tela de visitantes

--- python/controle/controle_visitantes/urls.py
+++ python/controle/controle_visitantes/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from dashboard.views import index
-from visitantes.views import registrar_visitante
+from visitantes.views import registrar_visitante, informacoes_visitante
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name="index"),
     path('registrar-visitante/', registrar_visitante, name="registrar_visitante"),
+    path(
+        'informacoes-visitante/<int:id>/',
+        informacoes_visitante,
+        name="informacoes_visitante"
+    )
 ]
